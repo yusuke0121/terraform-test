231a6b79acec3a887acb4c55d9af88171b70e32d
YusukeNakakura <yusukenakakura@gmail.com>
2021-09-15 02:24:34 +0900

    second-commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# vpc作成
+#　vpc作成
 resource "aws_vpc" "tfvpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -9,7 +9,7 @@ resource "aws_vpc" "tfvpc" {
   }  
 }
 
-# IGW
+#　IGW
 resource "aws_internet_gateway" "tfigw" {
   vpc_id = aws_vpc.tfvpc.id
 
@@ -18,6 +18,7 @@ resource "aws_internet_gateway" "tfigw" {
   }
 }
 
+#　サブネット作成
 resource "aws_subnet" "tfsubnet" {
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-northeast-1a"
@@ -29,6 +30,7 @@ resource "aws_subnet" "tfsubnet" {
   }
 }
 
+#　ルートテーブル
 resource "aws_route_table" "tfroutetable" {
   vpc_id = aws_vpc.tfvpc.id
 
@@ -37,17 +39,20 @@ resource "aws_route_table" "tfroutetable" {
   }
 }
 
+#　ルート
 resource "aws_route" "tfroute" {
   route_table_id         = aws_route_table.tfroutetable.id
   gateway_id             = aws_internet_gateway.tfigw.id
   destination_cidr_block = "0.0.0.0/0"  
 }
 
+#　IGWへアタッチ
 resource "aws_route_table_association" "tfassociation" {
   subnet_id      = aws_subnet.tfsubnet.id
   route_table_id = aws_route_table.tfroutetable.id
 }
 
+#　セキュリティグループ
 resource "aws_security_group" "tfsg" {
    vpc_id = aws_vpc.tfvpc.id
    
@@ -56,6 +61,7 @@ resource "aws_security_group" "tfsg" {
    }
 }
 
+#　インバウンド22
 resource "aws_security_group_rule" "tfrule22" {
   security_group_id = aws_security_group.tfsg.id 
   type        = "ingress"
@@ -65,6 +71,7 @@ resource "aws_security_group_rule" "tfrule22" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+#　インバウンド80
 resource "aws_security_group_rule" "tfrule80" {
   security_group_id = aws_security_group.tfsg.id     
   type        = "ingress"
@@ -74,11 +81,22 @@ resource "aws_security_group_rule" "tfrule80" {
   cidr_blocks = ["0.0.0.0/0"] 
 }
 
+# アウトバウンド開放
+resource "aws_security_group_rule" "out_all" {
+  security_group_id = aws_security_group.tfsg.id
+  protocol          = "-1" 
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+
+# keypair
 resource "aws_key_pair" "example1" {
   key_name = "example1"
-  public_key = file("~/example1.pub")
+  public_key = file("~/.ssh/example1.pub")
 }
 
+# EC2インスタンス
 resource "aws_instance" "tfinstance" {
   ami                         = "ami-00d101850e971728d"
   vpc_security_group_ids      = [aws_security_group.tfsg.id]
